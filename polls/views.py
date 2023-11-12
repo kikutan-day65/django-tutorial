98ba2f7e260fc36b07ee98d8a76fc45808b1a538
Haruto Mori <baystarsman@outlook.jp>
2023-11-12 15:47:47 +0900

    add some view finctions and its urls

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,16 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello world")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def results(request, question_id):
+    response = f"You're looking at question {question_id}"
+    return HttpResponse(f"response {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
